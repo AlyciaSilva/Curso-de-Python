4bdfe3c05cac22b5a2a1bab9770e1aadb623714a
Alycia Silva <alycialsilvaprogrammer@gmail.com>
2021-04-25 12:53:17 -0300

    exercicios de modulos e pacotes

---
+++ ModulosEPacotes/Exerc107P/moeda.py
@@ -0,0 +1,11 @@
+#Exercicio 107
+def aumentar(n,p):
+    t = n + (n * p/100)
+    return t
+def diminuir(n,p):
+    total = n  - (n * p/100)   
+    return total
+def dobro(n):
+    return n*2
+def metade(n):
+    return n/2
