8b10fca2aed4708e5592f493cab638dce0b86439
Alycia <alycialsilvaprogrammer@gmail.com>
2021-04-25 18:51:57 -0300

    exercicio de modulo

--- ModulosEPacotes/Exerc108.py
+++ ModulosEPacotes/Exerc108.py
@@ -1,7 +1,7 @@
 from Exerc107P import moeda
 
 p = float(input("Escreva o preço: "))
-print(f'a metade de {p} é {moeda.metade(p)}')
-print(f'o dobro é {moeda.dobro(p)}')
-print(f'o aumento com 10% é {moeda.aumentar(p,10)}')
-print(f'a diminuição com 13% é de {moeda.diminuir(p,13)}')
+print(f'a metade de {p} é {moeda.moeda(moeda.metade(p))}') 
+print(f'o dobro é {moeda.moeda(moeda.dobro(p))}') 
+print(f'o aumento com 10% é {moeda.moeda(moeda.aumentar(p,10))}') 
+print(f'a diminuição com 13% é de {moeda.moeda(moeda.diminuir(p,13))}')
